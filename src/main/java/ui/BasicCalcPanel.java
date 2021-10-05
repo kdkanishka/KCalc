5297e76feade3567f90a71830e1b629e64de04ba
Kanishka Dilshan <kanishka.desilva@pagero.com>
2021-10-05 10:37:17 +0530

    Few adjustments to ui

--- src/main/java/ui/BasicCalcPanel.java
+++ src/main/java/ui/BasicCalcPanel.java
@@ -14,43 +14,54 @@ public class BasicCalcPanel extends JPanel {
     public BasicCalcPanel() {
         setLayout(new FlowLayout());
 
+        add(new JLabel("Expression"));
+
         TextField display = new TextField();
         display.setText("");
         display.setColumns(18);
 
         Font calcFont=new Font("Monospaced", Font.BOLD, 20);
+        Font resultFont=new Font("Monospaced", Font.BOLD, 16);
         display.setFont(calcFont);
 
 
         add(display);
-        JButton calcButton = new JButton("Calc");
+        JButton clearButton = new JButton("Clear");
+        JButton calcButton = new JButton("Evaluate");
         add(calcButton);
+        add(clearButton);
 
-        JTextArea jTextArea = new JTextArea(7,18);
-        jTextArea.setFont(calcFont);
+        JTextArea jTextArea = new JTextArea(7,22);
+        jTextArea.setFont(resultFont);
         jTextArea.setForeground(Color.BLUE);
         jTextArea.setBorder(BorderFactory.createLineBorder(Color.GRAY));
-        add(jTextArea);
+//        add(jTextArea);
 
-        //add click events
-        calcButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    String expression = display.getText();
-                    System.out.println(expression);
-                    CalcExpression calcExpression = new CalcExpression(expression);
-                    CalcExpressionEvaluator evaluator = new CalcExpressionEvaluator();
-                    double result = evaluator.evaluate(calcExpression);
-                    display.setText("");
+        JScrollPane scrollBar=new JScrollPane(jTextArea,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        add(scrollBar);
 
-                    jTextArea.append(expression+ " = " +result+"\n");
-                }catch (InvalidExpressionException ex){
-                    JOptionPane.showMessageDialog(null,
-                            ex.getMessage(),
-                            "Evaluation Failure",
-                            JOptionPane.WARNING_MESSAGE);
-                }
+
+        //add click event for clear
+        clearButton.addActionListener(e -> {
+            display.setText("");
+        });
+
+        //add click event for evaluation
+        calcButton.addActionListener(e -> {
+            try {
+                String expression = display.getText();
+                System.out.println(expression);
+                CalcExpression calcExpression = new CalcExpression(expression);
+                CalcExpressionEvaluator evaluator = new CalcExpressionEvaluator();
+                double result = evaluator.evaluate(calcExpression);
+                display.setText("");
+
+                jTextArea.append(expression+ "=" +result+"\n");
+            }catch (InvalidExpressionException ex){
+                JOptionPane.showMessageDialog(null,
+                        ex.getMessage(),
+                        "Evaluation Failure",
+                        JOptionPane.WARNING_MESSAGE);
             }
         });
     }
